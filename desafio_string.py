f94bf1aaa1e69eb87142e772f93a75c3c63c0e13
Sebastian Mora <sebastian.mt99@gmail.com>
2023-03-11 22:53:21 -0500

    ejercicio creando una cadena

---
+++ desafio_string.py
@@ -0,0 +1,6 @@
+cadena_1 = "versátil"
+cadena_2 = "Python"
+cadena_3 = "es un lenguaje"
+cadena_4 = "de programación"
+nueva_cadena = cadena_2 + ' ' + cadena_3 + ' ' + cadena_4 + ' ' + cadena_1
+print(nueva_cadena)
