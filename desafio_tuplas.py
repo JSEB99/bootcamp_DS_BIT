1ff7b96aafc1d08e3afa8d60e3e4df0b5f62c3b3
Sebastian Mora <sebastian.mt99@gmail.com>
2023-03-13 21:55:51 -0500

    editando valor posicion 8 contando desde 1

--- desafio_tuplas.py
+++ desafio_tuplas.py
@@ -3,6 +3,6 @@ ultimo_item_tupla = tupla[-1]
 items_tupla = len(tupla)
 item_87 = list(tupla).index(87)
 ultimos_3_items = list(tupla)[-3:]
-item_pos_8 = tupla[8]
+item_pos_8 = tupla[7] #desde 1
 veces_7 = list(tupla).count(7)
 print(ultimo_item_tupla,items_tupla,item_87,ultimos_3_items,item_pos_8,veces_7)
