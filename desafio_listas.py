4f7aaa94aac3138facdb44c801bb151c166f3551
Sebastian Mora <sebastian.mt99@gmail.com>
2023-03-13 21:01:24 -0500

    trabajando con listas

---
+++ desafio_listas.py
@@ -0,0 +1,8 @@
+lista1=[1,2,3]
+lista2=[4,5,6]
+lista1.append(456789)
+lista1.append("Hola Mundo")
+lista3=lista1[:-1]
+lista4=lista2[1:-1]
+lista5=lista4+lista3
+print(lista1,lista2,lista3,lista4,lista5)
